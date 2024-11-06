024bc9f081bfc7da79315b34fde910ba2a9fc56f
Adam Thomas <bobdude2@hotmail.co.uk>
2024-11-06 00:02:22 +0000

    Constructing the ASC and AS

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -13,6 +13,11 @@ AAuraCharacterBase::AAuraCharacterBase()
 
 }
 
+UAbilitySystemComponent* AAuraCharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
 // Called when the game starts or when spawned
 void AAuraCharacterBase::BeginPlay()
 {
