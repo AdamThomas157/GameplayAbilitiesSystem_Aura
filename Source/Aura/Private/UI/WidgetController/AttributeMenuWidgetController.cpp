70cabb638e993b59a3edf6a683d7cc7d7a3fa441
Adam Thomas <bobdude2@hotmail.co.uk>
2025-02-04 17:23:23 +0000

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Adam Thomas
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
