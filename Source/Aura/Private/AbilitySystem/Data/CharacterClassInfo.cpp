6cf5466077783d86aa0223ac5fc9a86597aa0a7d
Adam Thomas <bobdude2@hotmail.co.uk>
2025-04-01 18:06:20 +0100

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Adam Thomas
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+    return CharacterClassInformation.FindChecked(CharacterClass);
+}
