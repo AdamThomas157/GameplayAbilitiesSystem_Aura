a75700681b75c1d36b52fb69ecf15da8078b480f
Adam Thomas <bobdude2@hotmail.co.uk>
2025-06-08 16:00:00 +0100

    Implementing Armour and Armour Penetration

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -9,12 +9,14 @@
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armour);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmourPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armour, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, ArmourPenetration, Source, false);
 	}
 };
 
@@ -28,6 +30,7 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmourDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmourPenetrationDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
@@ -43,7 +46,6 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
 	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
-
 	FAggregatorEvaluateParameters EvaluationParameters;
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
@@ -56,7 +58,6 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	float TargetBlockChance = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
-
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
 
 	// Create a const boolean to handle random range then use that to determine if a block occurs with TargetBlockChance
@@ -64,9 +65,32 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	// Set Damage to be a tertinary operation of if bBlocked is true halve the damage 
 	// otherwise set it to original attribute value
 	
+	// In a gameplay effect execution calculation, order of calculations matter
+	// So if Blocked and halves the incoming damage, this affects the rest of the subsequent calculations
+
 	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
 
+	// If bBlocked is true, damage is halved
 	Damage = bBlocked ? Damage / 2.f : Damage;
+
+	// Armour and Armour Penetration implementation
+	// Target Armour, Source Armour Penetration
+
+	float TargetArmour = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmourDef, EvaluationParameters, TargetArmour);
+	TargetArmour = FMath::Max<float>(TargetArmour, 0.f);
+
+	float SourceArmourPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmourPenetrationDef, EvaluationParameters, SourceArmourPenetration);
+	SourceArmourPenetration = FMath::Max<float>(SourceArmourPenetration, 0.f);
+
+	
+	// EffectiveArmour will store the percentage of the Target's Armour
+	// Armour Penetration ignores a percentage of the Target's Armour
+	const float EffectiveArmour = TargetArmour *= (100 - SourceArmourPenetration * 0.25f) / 100.f;
+
+	// Armour ignores a percentage of incoming damage
+	Damage *= (100 - EffectiveArmour * 0.333f) / 100.f;
 
 	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecuteOutput.AddOutputModifier(EvaluatedData);
