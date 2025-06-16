76dcfbcc5bace9090ba851391c0397e760c143cf
Adam Thomas <bobdude2@hotmail.co.uk>
2025-06-16 15:52:46 +0100

    AI Controller Blackboard and Behavior Tree

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,16 @@
+// Copyright Adam Thomas
+
+
+#include "AI/AuraAIController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AAuraAIController::AAuraAIController()
+{
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+}
